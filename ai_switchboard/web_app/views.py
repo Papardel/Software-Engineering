334d40f442fb22171f3ff8a919f1c3c913bf7793
victor <victor.victor407@yahoo.ro>
2024-03-21 13:18:46 +0100

    redid the whole thing, base front up and running

--- ai_switchboard/web_app/views.py
+++ ai_switchboard/web_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    days_left = 12
+    return render(request, 'home.html', {'days_left': days_left})
