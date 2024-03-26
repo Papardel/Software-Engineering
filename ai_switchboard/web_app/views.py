4b7da76c140bc9518a8b26c7071be21636418e9e
Vasco Reynolds Brandão <vasco@reynoldsbrando.com>
2024-03-26 15:19:49 +0100

    views package

--- ai_switchboard/web_app/views.py
+++ ai_switchboard/web_app/views.py
@@ -1,14 +1,10 @@
-import base64
-import tempfile
 import logging
-
 from django.contrib.auth.decorators import login_required
-
-from . import mediapipe_app
 from .viewslib.user_login_view import *
 from .viewslib.download_view import *
 from .viewslib.upload_view import *
 from .viewslib.media_view import *
+from .viewslib.ai_processing_view import *
 
 logger = logging.getLogger(__name__)
 
@@ -50,25 +46,7 @@ def download(request):
 
 @login_required
 def process_video_view(request, vid_name=None):
-    if vid_name is None:
-        # Fetch all video names from the database
-        videos = Video.objects.all()
-        # Render a template that displays all video names
-        return render(request, 'process_video.html', {'videos': videos})
-    else:
-        # Fetch the video with the given name from the database
-        video = Video.objects.get(name=vid_name)
-
-        # Write the video data to a temporary file
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            # Decode the video data to bytes if it's a base64 string
-            video_data = base64.b64decode(video.data) if isinstance(video.data, str) else video.data
-            temp_file.write(video_data)
-            temp_file_path = temp_file.name
-
-        # Call the process_video function and pass the video id, temporary file path, and video name
-        mediapipe_app.process_video(video.id, temp_file_path, vid_name)
-        return HttpResponse('Video analysis initiated', status=200)
+    return mediapipe_video_logic(request, vid_name)
 
 
 @login_required
---
+++ ai_switchboard/web_app/viewslib/ai_processing_view.py
@@ -0,0 +1,30 @@
+import base64
+import tempfile
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from ai_switchboard.web_app import mediapipe_app
+from ai_switchboard.web_app.models import *
+
+
+def mediapipe_video_logic(request, vid_name=None):
+    if vid_name is None:
+        # Fetch all video names from the database
+        videos = Video.objects.all()
+        # Render a template that displays all video names
+        return render(request, 'process_video.html', {'videos': videos})
+    else:
+        # Fetch the video with the given name from the database
+        video = Video.objects.get(name=vid_name)
+
+        # Write the video data to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            # Decode the video data to bytes if it's a base64 string
+            video_data = base64.b64decode(video.data) if isinstance(video.data, str) else video.data
+            temp_file.write(video_data)
+            temp_file_path = temp_file.name
+
+        # Call the process_video function and pass the video id, temporary file path, and video name
+        mediapipe_app.process_video(video.id, temp_file_path, vid_name)
+        return HttpResponse('Video analysis initiated', status=200)
