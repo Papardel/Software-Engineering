abf15a47a575eb816795b6e0464be74a1afeede4
victor <victor.victor407@yahoo.ro>
2024-03-29 15:52:41 +0100

    attempt at fixing remote live stream

--- ai_switchboard/web_app/viewslib/live_feed_view.py
+++ ai_switchboard/web_app/viewslib/live_feed_view.py
@@ -7,45 +7,57 @@ from django.utils import timezone
 
 from ..models import *
 
+import cv2
 
-# tag compresses response as gzip -> streaming data efficiently over network
+
+def find_available_cameras(max_cameras_to_check=10):
+    available_cameras = []
+    for i in range(max_cameras_to_check):
+        cap = cv2.VideoCapture(i)
+        if cap.isOpened():
+            available_cameras.append(i)
+        else:
+            print(f"Cannot open camera at index {i}. Please ensure that the camera is connected and accessible.")
+        cap.release()
+        time.sleep(0.1)  # Add a small delay
+    return available_cameras
+
+
 @gzip.gzip_page
 def live_feed_logic(request):
     def generate_frames():
-        # 0 for default camera
-        cap = cv.VideoCapture(0)
+        available_cameras = find_available_cameras()
+        if not available_cameras:
+            return
+
+        cap = cv.VideoCapture(available_cameras[0])
+
         frame_count = 0
         video_buffer = []
         start_time = time.time()
 
         while True:
             if time.time() - start_time > 20:
-                # release webcam and output
                 cap.release()
                 break
             ret, frame = cap.read()
-            # can't receive frame
             if not ret:
                 break
 
-            # process frame
             _, img_encoded = cv.imencode('.jpg', frame)
             video_buffer.append(img_encoded.tobytes())
             frame_count += 1
 
-            # save 3-second videos every 90 frames (assuming 30 fps)
             if frame_count == 90:
                 save_video(video_buffer)
                 frame_count = 0
                 video_buffer = []
 
-            # yields encoded frame as a bytes object
             yield b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + img_encoded.tobytes() + b'\r\n'
 
     return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
 
 
-# save video to database
 def save_video(video):
     video_data = b''.join(video)
     new_video = Video.objects.create(
@@ -55,6 +67,16 @@ def save_video(video):
     new_video.save()
 
 
-# display livestream on the web app
 def show_live_stream(request):
-    return render(request, 'live_feed.html')
+    context = {
+        'camera_access_script': """
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function(stream) {
+                    /* Use the stream */
+                })
+                .catch(function(err) {
+                    /* Handle the error */
+                });
+        """
+    }
+    return render(request, 'live_feed.html', context)
