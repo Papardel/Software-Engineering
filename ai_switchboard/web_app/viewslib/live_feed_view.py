35622f12ea3fd9e539d1f8da74183992ed0d1616
victor <victor.victor407@yahoo.ro>
2024-04-24 14:28:55 +0200

    changed to async works babyyy

--- ai_switchboard/web_app/viewslib/live_feed_view.py
+++ ai_switchboard/web_app/viewslib/live_feed_view.py
@@ -1,4 +1,12 @@
+import asyncio
+import queue
+import re
+import threading
+import time
+import asgiref
+import httpx
 import requests
+from asgiref.sync import async_to_sync, sync_to_async
 from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from django.utils import timezone
@@ -7,41 +15,62 @@ from ..models import Video
 # Set this to the URL of the .m3u8 file provided by your Nginx HLS setup.
 nginx_hls_url = "http://192.168.3.249:8080/hls/stream.m3u8"
 
+video_queue = queue.Queue()
 
-def generate_frames():
-    print(f"Attempting to connect to {nginx_hls_url}")
-    r = requests.get(nginx_hls_url, stream=True)
-    if r.status_code == 200:
-        print("Connected successfully")
-        # Handling HLS stream: parse the .m3u8 playlist and manage segment downloads
-        for line in r.iter_lines():
-            if line and not line.startswith(b'#'):
-                # Assuming line is a path to a segment file
-                segment_url = nginx_hls_url.rsplit('/', 1)[0] + '/' + line.decode('utf-8')
-                segment_response = requests.get(segment_url, stream=True)
-                for chunk in segment_response.iter_content(chunk_size=1024):
-                    yield chunk
-    else:
-        print(f"Failed to get stream with status code: {r.status_code}")
+def save_video_thread():
+    while True:
+        video = video_queue.get()  # Get the next video from the queue
+        save_video(video)  # Save the video
+        video_queue.task_done()  # Indicate that the task is done
+
+def live_feed(request):
+    sync_live_feed_logic = async_to_sync(live_feed_logic)
+    response = sync_live_feed_logic(request)
+    return StreamingHttpResponse(response, content_type='multipart/x-mixed-replace; boundary=frame')
 
 
-def live_feed_logic(request):
+async def live_feed_logic(request):
     print("Accessing live feed from NGINX...")
     video_buffer = []
     frame_count = 0
+    desired_frame_count = 5  # Desired frame count
 
-    for chunk in generate_frames():
-        print("Received chunk")
-        video_buffer.append(chunk)
+    async for frame in generate_frames("http://192.168.3.249:8080/hls/stream.m3u8"):
+        print("Received frame")
+        video_buffer.append(frame)
         frame_count += 1
-        # Adjust this value according to your segment duration and logic needs
-        if frame_count >= 1000:
-            save_video(video_buffer)
+        if frame_count >= desired_frame_count:
+            # Add the video buffer to the queue
+            video_queue.put(video_buffer)
             video_buffer = []
             frame_count = 0
 
     return StreamingHttpResponse(iter([]), content_type='multipart/x-mixed-replace; boundary=frame')
 
+
+async def generate_frames(nginx_hls_url):
+    print(f"Attempting to connect to {nginx_hls_url}")
+    async with httpx.AsyncClient() as session:
+        while True:
+            r = await session.get(nginx_hls_url)
+
+            if r.status_code == 200:
+                print("Connected successfully")
+                content = r.content.decode('utf-8')
+                segment_urls = [nginx_hls_url.rsplit('/', 1)[0] + '/' + 'stream-' + segment for segment in
+                                re.findall(r'(\w+\.ts)', content)]
+
+                for segment_url in segment_urls:
+                    segment_response = await session.get(segment_url)
+                    for frame in segment_response.iter_bytes():
+                        yield frame
+
+            else:
+                print(f"Failed to get stream with status code: {r.status_code}")
+                print("Retrying in 5 seconds...")
+                print(threading.enumerate())
+
+            await asyncio.sleep(5)  # Add delay before retrying
 
 def save_video(video):
     video_data = b''.join(video)
