f6bfdcad112959b6dd388bfe7c44ca0338858215
victor <victor.victor407@yahoo.ro>
2024-05-11 21:26:16 +0200

    frontend lets user choose feed

--- ai_switchboard/web_app/viewslib/stream_renderer_view.py
+++ ai_switchboard/web_app/viewslib/stream_renderer_view.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from dotenv import load_dotenv
 import os
 
+from web_app.models import Camera
+
 load_dotenv()
 
 
 def show_live_stream_view(request):
     hls_url = os.getenv('NGINX_HLS_URL')
     print(f"HLS URL FOR RENDER: {hls_url}")  # Print the HLS URL for debugging
-    return render(request, 'live_feed.html', {'hls_url': hls_url})
+
+    # Fetch all camera names from the database
+    cameras = Camera.objects.values_list('name', flat=True)
+
+    return render(request, 'live_feed.html', {'hls_url': hls_url, 'cameras': cameras})
