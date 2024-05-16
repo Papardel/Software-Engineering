0b647bde9db1e398d837f2a1f386d2b2f37c5d05
Vasco Reynolds Brandão <vasco@reynoldsbrando.com>
2024-05-16 19:56:08 +0200

    selecting data formats based on processing methods and then showing appropriate media and processing options works

--- ai_switchboard/web_app/viewslib/ai_processing_view.py
+++ ai_switchboard/web_app/viewslib/ai_processing_view.py
@@ -43,60 +43,71 @@ def get_video__make_temp_file(vid_name, processing_method):
     return video_file_path
 
 
-def media_format_logic(request):
-    if request.method == 'POST':
-        media = request.POST['fileType']
+# returns a dictionary of processing models for each file format that has processing models
+def get_processing_models():
+    return {k: v for k, v in processor_dictionary.items() if v != {}}
 
-        if media is None:
-            return render(request, 'process.html')
-        else:
-            match media:
-                case 'video':
-                    return render(request, 'process.html', {'media': Video.objects.all(),
-                                                            'models': video_processor_dictionary.keys()})
-                case 'image':
-                    return render(request, 'process.html', {'media': Image.objects.all(),
-                                                            'models': image_processing_dictionary.keys()})
-                case 'json':
-                    return render(request, 'process.html', {'media': JSON.objects.all(),
-                                                            'models': json_processing_dictionary.keys()})
-                case 'csv':
-                    return render(request, 'process.html', {'media': CSV.objects.all(),
-                                                            'models': csv_processing_dictionary.keys()})
-                case 'txt':
-                    return render(request, 'process.html', {'media': Text.objects.all(),
-                                                            'models': txt_processing_dictionary.keys()})
-                case _:
-                    return render(request, 'process.html')
-    return render(request, 'process.html')
+
+def find_format(processing_model):
+    for data_format in processor_dictionary:
+        if processing_model in processor_dictionary[data_format].keys():
+            return processor_dictionary[data_format]
+    return None
+
+
+def get_data_format_object(data_format):
+    match data_format:
+        case 'video':
+            return Video
+        case 'image':
+            return Image
+        case 'json':
+            return JSON
+        case 'csv':
+            return CSV
+        case 'txt':
+            return Text
+        case _:
+            return None
+
+
+def media_format_logic(request):
+    format_processing_models = get_processing_models()
+    formats = list(format_processing_models.keys())
+    def_models = list(format_processing_models[formats[0]].keys())
+    def_media = get_data_format_object(formats[0]).objects.all()
+
+    if request.method == 'POST':
+        chosen_format = request.POST['fileType']
+        models = format_processing_models[chosen_format]
+        media = get_data_format_object(chosen_format).objects.all()
+        return render(request, 'process.html', {'formats': formats, 'models': models, 'media': media})
+
+    return render(request, 'process.html', {'formats': formats, 'models': def_models, 'media': def_media})
 
 
 """ 
-Later we will need to handle the case where the user clicks on the file when no processing
-method is selected and hence a popup shows up or a message is displayed to the user. Another option is to 
-only display file_types which have processing methods available.
+Based on the available processing methods for each data format, the process.html page will display the available
+formats and processing methods for the user to choose from. The user can then select a format and processing method
+as well as media of that format to process. 
 """
 
 
 def ai_processing_logic(request, file_name=None, processing_model=None):
     if file_name is None:
-        """Later we will include the data_type format which conditions the rest but right now, everything is a video."""
-        # return media_format_logic(request)
-        videos = Video.objects.all()
-        return render(request, 'process.html', {'media': videos,
-                                                'models': video_processor_dictionary.keys()})
+        return media_format_logic(request)
     else:
-        """
-        Eventually the dictionary used below will by the format chosen by the user and from there a subset 
-        of processing methods will be provided to the user to choose from. 
-        """
-        # make temp file of file retrieved from db
-        get_video__make_temp_file(file_name, video_processor_dictionary[processing_model].get_directory())
-        output_name = video_processor_dictionary[processing_model].run_model(file_name)  # run model
+        processing_dictionary = find_format(processing_model)
+
+        # make temp file of file retrieved from db in the directory of the processing model
+        get_video__make_temp_file(file_name, processing_dictionary[processing_model].get_directory())
+        output_name = processing_dictionary[processing_model].run_model(file_name)  # run model
 
         Notification.objects.create(
-            message=f'User {request.user.username} processed file {file_name}',
+            message=f'User {request.user.username} processed file {file_name} with {processing_model}',
             user=request.user
         )
-        return HttpResponse(f'{processing_model} processing complete, check the media section for {output_name}',
+
+        """HTTP Response needs to be changed to a pop notification or a redirect to the media page."""
+        return HttpResponse(f'{processing_model} processing complete on {file_name}, check the media section for {output_name}',
                             status=200)
