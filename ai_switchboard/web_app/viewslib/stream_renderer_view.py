70da6f0a6edf61b5023eb1fbc61b8d3773a54139
victor <victor.victor407@yahoo.ro>
2024-05-29 21:26:16 +0200

    stream fix

--- ai_switchboard/web_app/viewslib/stream_renderer_view.py
+++ ai_switchboard/web_app/viewslib/stream_renderer_view.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from dotenv import load_dotenv
 import os
-import logging
 
-logger = logging.getLogger(__name__)
-from ..models import Camera
+from web_app.models import Camera
 
 load_dotenv()
 
 
 def show_live_stream_view(request):
-    hls_url = os.getenv('NGINX_HLS_URL')
-    logger.info(f"HLS URL FOR RENDER: {hls_url}")  # Print the HLS URL for debugging
-
+    base_hls_url = os.getenv('NGINX_HLS_URL')
     # Fetch all camera names from the database
     cameras = Camera.objects.values_list('name', flat=True)
 
-    return render(request, 'live_feed.html', {'hls_url': hls_url, 'cameras': cameras})
+    # Create a dictionary with camera names as keys and HLS URLs as values
+    hls_urls = {camera: base_hls_url.format(camera_name=camera) for camera in cameras}
+    print(f"HLS URLs FOR RENDER: {hls_urls}")  # Print the HLS URLs for debugging
+
+    return render(request, 'live_feed.html', {'hls_urls': hls_urls, 'cameras': cameras})
